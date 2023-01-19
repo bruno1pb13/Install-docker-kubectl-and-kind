2986b903f8a433df21e0d01d2302eec27a533b2b
Bruno S <bruno1pb13@gmail.com>
2023-01-19 09:22:28 -0300

    Update installBundle.sh

--- installBundle.sh
+++ installBundle.sh
@@ -2,81 +2,81 @@
 
 divider='======================================'
 
-echo 'Atualizando o sistema'
+echo 'Updating your system'
 sudo apt update
 
 echo $divider
 
 if ! [ -x "$(command -v curl)" ]; then
-  echo 'Instalando o curl'
+  echo 'Installing curl'
   sudo apt install curl
 else
-    echo 'Curl instalado, seguindo para o proximo passo'
+    echo 'Curl Install successful, going to next step'
 fi
 
 echo $divider
 
 if ! [ -x "$(command -v docker)" ]; then
-    echo 'Instalando o Docker'
+    echo 'Installing Docker'
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
 else
-    echo 'Docker instalado, seguindo para o proximo passo'
+    echo 'Docker Install successful, going to next step'
 fi
 
 echo $divider
 
 
 if ! [ -x "$(command -v kubectl)" ]; then
-    echo 'Instalando o kubectl'
+    echo 'Installing kubectl'
     curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
     chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
 else
-    echo 'kubectl instalado, seguindo para o proximo passo'
+    echo 'kubectl Install successful, going to next step'
 fi
 
 echo $divider
 
 
 if ! [ -x "$(command -v kubectl)" ]; then
-        echo 'Impossivel instalar o kubectl :(, tente instalar manualmente'
+        echo 'Unable to install kubectl :(, try installing manually'
         echo EOF
             curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
             chmod +x ./kubectl
             sudo mv ./kubectl /usr/local/bin/kubectl
         EOF
 else
-    echo 'kubectl instalado, seguindo para o proximo passo'
+    echo 'kubectl Install successful, going to next step'
 fi
 
 echo $divider
 
 
 if ! [ -x "$(command -v kind)" ]; then
-    echo 'Instalando o kind'
+    echo 'Installing kind'
     curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
     chmod +x ./kind
     sudo mv ./kind /usr/local/bin/kind
 else
-    echo 'kind instalado, seguindo para o proximo passo'
+    echo 'kind Install successful, going to next step'
 fi
 
 install=true
 
 while true; do
     echo $divider
-    read -p "Deseja configurar o autocomplete para o kubectl? (Y/n) " yn
+    read -p "Do you want to configure autocompletion for kubectl? (Y/n) " yn
     case $yn in
         [Yy]* ) 
-            echo 'Instalando o bash-completion'
+            echo 'Installing bash-completion'
             sudo apt install bash-completion
             break;;
         [Nn]* ) 
             install=false
             break;;
         * ) 
-            echo 'Instalando o bash-completion'
+            echo 'Installing bash-completion'
             sudo apt install bash-completion
             break;;
     esac
@@ -87,7 +87,7 @@ then
 echo $divider
 
     while true; do
-        read -p "Deseja instalar para qual terminal? Bash (b) ou ZSH (z)? " yn
+        read -p "Which terminal do you want to install to? Bash (b) ou ZSH (z)? " yn
         case $yn in
             [Bb]* ) 
                 mkdir ~/.kube
@@ -97,25 +97,25 @@ echo $divider
                 source '$HOME/.kube/completion.bash.inc'
                 " >> $HOME/.bash_profile
                 source $HOME/.bash_profile 
-                echo 'Instalado com sucesso, reinicie o terminal para ativar o autocomplete'
+                echo 'Installed successfully, restart terminal to enable autocomplete'
                 break;;
             [Zz]* ) 
                 # Load the kubectl completion code for zsh[1] into the current shell
                 source <(kubectl completion zsh)
                 # Set the kubectl completion code for zsh[1] to autoload on startup
                 kubectl completion zsh > "${fpath[1]}/_kubectl"
-                echo 'Instalado com sucesso, reinicie o terminal para ativar o autocomplete'
+                echo 'Installed successfully, restart terminal to enable autocomplete'
                 break;;
             * ) 
-                echo "use (b) para bash ou (z) para zsh.";;
+                echo "use (b) to bash or (z) to zsh.";;
         esac
     done
 fi
 
 echo $divider
-echo 'Tudo rodando que é uma beleza, agora você já pode iniciar seus clusters com o kind'
-echo 'Duvidas sobre o docker acesse: https://docs.docker.com/engine/reference/commandline/cli/'
+echo 'Everything running beautifully, now you can start your clusters with kind'
+echo 'Questions about docker visit: https://docs.docker.com/engine/reference/commandline/cli/'
 echo ''
-echo 'Duvidas sobre o kubectl acesse: https://kubernetes.io/docs/reference/kubectl/overview/'
+echo 'Questions about kubectl visit : https://kubernetes.io/docs/reference/kubectl/overview/'
 echo ''
-echo 'Duvidas sobre o kind acesse: https://kind.sigs.k8s.io/docs/user/quick-start/'
+echo 'Questions about kind visit: https://kind.sigs.k8s.io/docs/user/quick-start/'
