db98e7b2986f57bfc9311434aee9eb4bc82ef0f6
Bruno S <bruno1pb13@gmail.com>
2023-01-19 09:34:03 -0300

    Update installBundle.sh

--- installBundle.sh
+++ installBundle.sh
@@ -101,13 +101,14 @@ echo $divider
                 break;;
             [Zz]* ) 
                 echo 'Unavaliable for now, try installing manually'
-                echo EOF 
+                #echo EOF 
                 # Load the kubectl completion code for zsh[1] into the current shell
-                source <(kubectl completion zsh)
+                #source <(kubectl completion zsh)
                 # Set the kubectl completion code for zsh[1] to autoload on startup
-                kubectl completion zsh > "${fpath[1]}/_kubectl"
-                echo 'Installed successfully, restart terminal to enable autocomplete'
-                EOF
+                #kubectl completion zsh > "${fpath[1]}/_kubectl"
+                #echo 'Installed successfully, restart terminal to enable autocomplete'
+                #EOF
+                
                 break;;
             * ) 
                 echo "use (b) to bash or (z) to zsh.";;
